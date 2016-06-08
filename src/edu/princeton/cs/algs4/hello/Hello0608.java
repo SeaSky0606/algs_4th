5824188a7dbf1690ec915cf77372caef8eb17d4f
junhong <847421525@qq.com>
2016-06-08 12:57:42 +0800

    this is eclipse test for git 'commit'

---
+++ src/edu/princeton/cs/algs4/hello/Hello0608.java
@@ -0,0 +1,10 @@
+package edu.princeton.cs.algs4.hello;
+
+public class Hello0608 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world!");
+	}
+
+}
